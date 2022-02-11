eb9eb2057703cc38d4867efb712780da22e5ad53
HyulyaG <hea2590@gmail.com>
2022-02-11 21:24:03 +0200

    Updated Second Branch

--- src/com/company/SecondClass.java
+++ src/com/company/SecondClass.java
@@ -3,5 +3,6 @@ package com.company;
 public class SecondClass {
     public static void main (String[] args) {
         System.out.println("New Page Second Class");
+        System.out.println("Update");
     }
 }
